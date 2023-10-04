2069e6765552d17f1596f9e2d4282cd882b3f120
Dalton Friend <52545724+DaltonFriend@users.noreply.github.com>
2023-10-04 22:23:06 +0100

    0.5

---
+++ javascript.js
@@ -0,0 +1,128 @@
+// Time
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("time").innerHTML = h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+
+// Cursor
+
+document.addEventListener("DOMContentLoaded", () => {
+  const circle = document.getElementById("circle");
+  const circleStyle = circle.style;
+  let isClicked = false;
+  let isHovered = false;
+
+  // Set the initial z-index value
+  circleStyle.zIndex = "9999";
+
+  document.addEventListener("mousemove", (e) => {
+    circleStyle.top = `${e.clientY - circle.offsetHeight / 2}px`;
+    circleStyle.left = `${e.clientX - circle.offsetWidth / 2}px`;
+
+    // Increase the z-index value to ensure the cursor is on top
+    circleStyle.zIndex = "9999";
+  });
+
+  const items = document.querySelectorAll(
+    ".social_container, .intro_content, .box"
+  );
+
+  items.forEach((item) => {
+    item.addEventListener("mouseover", hoverCircle);
+    item.addEventListener("mouseout", resetCircle);
+  });
+
+  document.addEventListener("mousedown", () => {
+    isClicked = true;
+    if (!isHovered) {
+      circle.style.transform = "scale(0.9)";
+    } else {
+      circle.style.transform = "scale(0.36)";
+    }
+  });
+
+  document.addEventListener("mouseup", () => {
+    isClicked = false;
+    if (isHovered) {
+      circle.style.transform = "scale(0.36)";
+    } else {
+      resetCircle();
+    }
+  });
+
+  function hoverCircle() {
+    circle.style.backgroundColor = "";
+    if (isClicked) {
+      circle.style.transform = "scale(0.36)";
+    } else {
+      circle.style.transform = "scale(0.4)";
+    }
+    isHovered = true;
+  }
+
+  function resetCircle() {
+    circle.style.backgroundColor = "";
+    if (!isClicked) {
+      circle.style.transform = "";
+      isHovered = false;
+    }
+  }
+});
+
+// mouseout cursor effects
+
+let fadeOut;
+
+document.addEventListener("mouseout", (e) => {
+  if (e.relatedTarget === null) {
+    fadeOut = setTimeout(() => {
+      circle.style.opacity = 0;
+    }, 0);
+  }
+});
+
+document.addEventListener("mouseover", () => {
+  clearTimeout(fadeOut);
+
+  circle.style.opacity = 1;
+});
+
+// Disable scrolling
+document.body.style.overflow = "hidden";
+
+// Re-enable scrolling
+document.body.style.overflow = "auto";
+
+document.ontouchmove = function (event) {
+  event.preventDefault();
+};
+
+const socialContainer = document.querySelector(".social_container");
+const socialTooltip = document.querySelector(".social_tooltip");
+
+socialContainer.addEventListener("mousemove", (e) => {
+  socialTooltip.style.left = e.clientX + "px";
+  socialTooltip.style.top = e.clientY + "px";
+});
+
+socialContainer.addEventListener("mouseleave", () => {
+  socialTooltip.style.display = "none";
+});
+
+socialContainer.addEventListener("mouseenter", () => {
+  socialTooltip.style.display = "block";
+});
